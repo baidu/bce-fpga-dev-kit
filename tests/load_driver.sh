a2688546c328c04c6707998ef90ff5c94021f6c4
miaotianxiang <miaotianxiang@baidu.com>
2017-06-08 17:13:39 +0800

    other: change driver name in tests/load_driver.sh

--- tests/load_driver.sh
+++ tests/load_driver.sh
@@ -15,12 +15,12 @@ echo -n "Loading driver..."
 # Use the following command to Load the driver in the default 
 # or interrupt drive mode. This will allow the driver to use 
 # interrupts to signal when DMA transfers are completed.
-insmod ../driver/xdma.ko
+insmod ../driver/xdma_xvc.ko
 # Use the following command to Load the driver in Polling
 # mode rather than than interrupt mode. This will allow the
 # driver to use polling to determ when DMA transfers are 
 # completed.
-#insmod ../driver/xdma.ko poll_mode=1
+#insmod ../driver/xdma_xvc.ko poll_mode=1
 
 if [ ! $? == 0 ]; then
   echo "Error: Kernel module did not load properly."
