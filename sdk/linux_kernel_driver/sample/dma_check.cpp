196896422d338e82b35958ae56712e8491e2b67d
miaotianxiang <miaotianxiang@baidu.com>
2017-07-11 17:08:30 +0800

    feature: add perf option to dma_check.cpp
    Change-Id: I3883b3ba72b513f94bfd21cd94c6e275daea6054

--- sdk/linux_kernel_driver/sample/dma_check.cpp
+++ sdk/linux_kernel_driver/sample/dma_check.cpp
@@ -16,16 +16,21 @@
 
 #include "fpga_cloud.h"
 #include <assert.h>
+#include "./timing.h"
 
 using namespace fpga::cloud::api;
+using fpga::rsa::api::StopWatch;
 
 bool g_failed = false;
+
+int g_perf = 0;
 
 struct thread_input_t {
     int i;
     size_t dma_size;
     uint64_t ddr_addr;
     uint64_t iteration;
+    uint64_t timecost_usec;
 };
 
 void *thread_func(void *p)
@@ -45,32 +50,64 @@ void *thread_func(void *p)
         return NULL;
     }
 
-    for (uint64_t i = 0; i < iteration; ++i) {
-        to_fpga[0] = (unsigned char)i;
-        to_fpga[dma_size - 1] = (unsigned char)(i + 1);
-
-        rc = fpga_memcpy(0, ddr_addr,
-                         reinterpret_cast<uint64_t>(to_fpga),
-                         dma_size, 1);
-        if (rc) {
-            g_failed = true;
-            fprintf(stderr, "fpga_memcpy failed, copy to fpga\n");
-            return NULL;
+    if (g_perf == O_RDONLY) {
+        StopWatch sw;
+        sw.start();
+        for (uint64_t i = 0; i < iteration; ++i) {
+            rc = fpga_memcpy(0, reinterpret_cast<uint64_t>(from_fpga),
+                    ddr_addr,
+                    dma_size, 0);
+            if (rc) {
+                g_failed = true;
+                fprintf(stderr, "fpga_memcpy failed, copy from fpga\n");
+                return NULL;
+            }
         }
-
-        rc = fpga_memcpy(0, reinterpret_cast<uint64_t>(from_fpga),
-                         ddr_addr,
-                         dma_size, 0);
-        if (rc) {
-            g_failed = true;
-            fprintf(stderr, "fpga_memcpy failed, copy from fpga\n");
-            return NULL;
+        sw.stop();
+        pargs->timecost_usec = sw.timecost_usec();
+    } else if (g_perf == O_WRONLY) {
+        StopWatch sw;
+        sw.start();
+        for (uint64_t i = 0; i < iteration; ++i) {
+            rc = fpga_memcpy(0, ddr_addr,
+                    reinterpret_cast<uint64_t>(to_fpga),
+                    dma_size, 1);
+            if (rc) {
+                g_failed = true;
+                fprintf(stderr, "fpga_memcpy failed, copy to fpga\n");
+                return NULL;
+            }
         }
+        sw.stop();
+        pargs->timecost_usec = sw.timecost_usec();
+    } else {
+        for (uint64_t i = 0; i < iteration; ++i) {
+            to_fpga[0] = (unsigned char)i;
+            to_fpga[dma_size - 1] = (unsigned char)(i + 1);
 
-        if (memcmp(to_fpga, from_fpga, dma_size)) {
-            g_failed = true;
-            fprintf(stderr, "thread %d data diff\n", i);
-            return NULL;
+            rc = fpga_memcpy(0, ddr_addr,
+                    reinterpret_cast<uint64_t>(to_fpga),
+                    dma_size, 1);
+            if (rc) {
+                g_failed = true;
+                fprintf(stderr, "fpga_memcpy failed, copy to fpga\n");
+                return NULL;
+            }
+
+            rc = fpga_memcpy(0, reinterpret_cast<uint64_t>(from_fpga),
+                    ddr_addr,
+                    dma_size, 0);
+            if (rc) {
+                g_failed = true;
+                fprintf(stderr, "fpga_memcpy failed, copy from fpga\n");
+                return NULL;
+            }
+
+            if (memcmp(to_fpga, from_fpga, dma_size)) {
+                g_failed = true;
+                fprintf(stderr, "thread %d data diff\n", i);
+                return NULL;
+            }
         }
     }
 
@@ -83,8 +120,8 @@ int main(int argc, char **argv)
 {
     int ret = 0;
 
-    if (argc != 4) {
-        fprintf(stderr, "Usage: %s <nthreads> <dma_size> <iteration>\n", argv[0]);
+    if (argc != 4 && argc != 5) {
+        fprintf(stderr, "Usage: %s <nthreads> <dma_size> <iteration> [perf, rd|wr]\n", argv[0]);
         return 1;
     }
 
@@ -103,6 +140,13 @@ int main(int argc, char **argv)
     if (*eof != '\0') {
         fprintf(stderr, "invalid iteration\n");
         return 1;
+    }
+    if (argc == 5) {
+        if (strncmp(argv[4], "rd", 2) == 0) {
+            g_perf = O_RDONLY;
+        } else if (strncmp(argv[4], "wr", 2) == 0) {
+            g_perf = O_WRONLY;
+        }
     }
 
     pthread_t threads[nthreads];
@@ -126,6 +170,16 @@ int main(int argc, char **argv)
         return 1;
     } else {
         fprintf(stderr, "dma_check OK\n");
+
+        if (g_perf) {
+            uint64_t total_usec = 0;
+            for (int i = 0; i < nthreads; ++i) {
+                total_usec += threads_input[i].timecost_usec;
+            }
+            double latency = (double)total_usec / nthreads / iteration;
+            double bandwidth = ((double)dma_size * iteration) / ((double)total_usec / nthreads);
+            printf("latency = %.2f, bandwidth = %.2f\n", latency, bandwidth);
+        }
         return 0;
     }
 }
