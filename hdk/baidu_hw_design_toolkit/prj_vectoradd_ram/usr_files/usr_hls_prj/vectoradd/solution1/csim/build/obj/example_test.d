5466ec1a2c8cc7bd1566a86ef6c1c9a05182744b
ruanyuan <ruanyuan@baidu.com>
2017-07-13 11:45:09 +0800

    delete hls prj's generated files

--- hdk/baidu_hw_design_toolkit/prj_vectoradd_ram/usr_files/usr_hls_prj/vectoradd/solution1/csim/build/obj/example_test.d
+++
@@ -1,6 +0,0 @@
-obj/example_test.o: ../../../../src/example_test.cpp \
- ../../../../src/example.h /home/opt/Vivado_HLS/2016.4/include/ap_int.h \
- /home/opt/Vivado_HLS/2016.4/include/etc/ap_int_sim.h \
- /home/opt/Vivado_HLS/2016.4/include/etc/ap_private.h \
- /home/opt/Vivado_HLS/2016.4/include/etc/ap_fixed_sim.h \
- /home/opt/Vivado_HLS/2016.4/include/etc/ap_int_sim.h
